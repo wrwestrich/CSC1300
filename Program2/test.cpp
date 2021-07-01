8ada7fa2d2491e98b4f933bce23b9698a92a5d0c
Will Westrich <wrwestrich@gmail.com>
2021-07-01 13:18:08 -0500

    Format Program 2

--- Program2/test.cpp
+++ Program2/test.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
-cout << "    _____" << endl;
-cout << "  .'     '." << endl;
-cout << " /  o   o  \\" << endl;
-cout << "|           |" << endl;
-cout << "|  \\     /  |" << endl;
-cout << " \\  '___'  /" << endl;
-cout << "  '._____.'" << endl;
+  cout << "    _____" << endl;
+  cout << "  .'     '." << endl;
+  cout << " /  o   o  \\" << endl;
+  cout << "|           |" << endl;
+  cout << "|  \\     /  |" << endl;
+  cout << " \\  '___'  /" << endl;
+  cout << "  '._____.'" << endl;
 
-cout << "    _____" << endl;
-cout << "  .'     '." << endl;
-cout << " /  o   o  \\" << endl;
-cout << "|           |" << endl;
-cout << "|   .___.   |" << endl;
-cout << " \\ /     \\ /" << endl;
-cout << "  '._____.'" << endl;
+  cout << "    _____" << endl;
+  cout << "  .'     '." << endl;
+  cout << " /  o   o  \\" << endl;
+  cout << "|           |" << endl;
+  cout << "|   .___.   |" << endl;
+  cout << " \\ /     \\ /" << endl;
+  cout << "  '._____.'" << endl;
 
-return 0;
+  return 0;
 }
